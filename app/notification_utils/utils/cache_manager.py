ea0f6a00e7624c3393cf5081cbedb64766e842e4
Amrit Saha <amritsaha607@gmail.com>
2024-02-07 17:06:10 +0530

    Refresh cache on update listener

--- app/notification_utils/utils/cache_manager.py
+++ app/notification_utils/utils/cache_manager.py
@@ -1,3 +1,4 @@
+import itertools
 import json
 
 from ..onboarding.models import PayloadMapping
@@ -12,13 +13,24 @@ def refresh_payload_mapping_pickup_conditions():
     for payload in PayloadMapping.objects.all():
         payload_id = payload.id
         mapping_json = json.loads(payload.mappingJSON)
-        payload_keys = list(mapping_json.keys())
+        payload_keys = get_payload_keys_from_mapping_json(mapping_json)
         pickup_conditions[payload_id] = payload_keys
+
+    print("Successfully refreshed payload mapping cache")
 
     PAYLOAD_MAPPING_PICKUP_CONDITIONS = pickup_conditions
 
     return PAYLOAD_MAPPING_PICKUP_CONDITIONS
 
 
-def refresh_cache(cache_key):
+def get_payload_keys_from_mapping_json(mapping_json: dict):
+    keys = mapping_json.values()
+    return list(set(itertools.chain.from_iterable([k.split(',') for k in keys])))
+
+
+def refresh_cache(cache_key=None):
+    if cache_key is None or cache_key.lower() == 'all':
+        refresh_payload_mapping_pickup_conditions()
+        return
+
     refresh_payload_mapping_pickup_conditions()
