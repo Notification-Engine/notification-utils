a6383553858796846c4b5e0cd6476b8eb6ceb25e
Amrit Saha <amritsaha607@gmail.com>
2024-02-28 09:49:38 +0530

    added AppDeviceSubscriptions model

--- app/notification_utils/onboarding/models/__init__.py
+++ app/notification_utils/onboarding/models/__init__.py
@@ -1,3 +1,4 @@
 from .app import Application
 from .device import Device
 from .payload import PayloadMapping
+from .subscription import AppDeviceSubscriptions
---
+++ app/notification_utils/onboarding/models/subscription.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+from .app import Application
+from .device import Device
+from .payload import PayloadMapping
+
+
+class AppDeviceSubscriptions(models.Model):
+    app = models.ForeignKey(
+        Application, on_delete=models.CASCADE, related_name='device_subscriptions')
+    device = models.ForeignKey(
+        Device, on_delete=models.CASCADE, related_name='app_subscriptions')
+    payload = models.ForeignKey(
+        PayloadMapping, on_delete=models.CASCADE, related_name='app_device_subscriptions')
+    is_subscribed = models.BooleanField()
+
+    class Meta:
+        app_label = 'Platform'
+        db_table = 'app_device_subscriptions'
+
+    def __str__(self):
+        return f"{self.app} - {self.device} | {self.payload} [{'Subscribed' if self.is_subscribed else 'Unsubscribed'}]"
+
+    def get_frontend_data(self):
+        payload_json = self.payload.to_dict()
+        return payload_json.update({
+            'is_subscribed': self.is_subscribed,
+        })
