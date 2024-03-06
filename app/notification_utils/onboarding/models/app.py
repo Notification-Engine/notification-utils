afd1601e43424e4c97a46ca36df95ed3725fe3b3
Amrit Saha <amritsaha607@gmail.com>
2024-03-06 18:30:40 +0530

    added android appid field & android utility methods in application model

--- app/notification_utils/onboarding/models/app.py
+++ app/notification_utils/onboarding/models/app.py
@@ -8,6 +8,7 @@ class Application(models.Model):
     password = models.CharField(max_length=256)
     subscriptions = models.ManyToManyField(
         'PayloadMapping', related_name='subscribed_apps')
+    android_app_id = models.CharField(max_length=64, null=True, blank=True)
     android_appname = models.CharField(max_length=30, null=True, blank=True)
     android_package = models.CharField(max_length=256, null=True, blank=True)
     created_at = models.DateTimeField(default=timezone.now)
@@ -26,3 +27,20 @@ class Application(models.Model):
             "name": self.name,
             "created_at": str(self.created_at)
         }
+
+    def get_android_details(self):
+        return {
+            'appId': self.appId,
+            'android_appname': self.android_appname,
+            'android_package': self.android_package,
+        }
+
+    def set_android_details(self, android_app_id, android_appname, android_package):
+        self.android_app_id = android_app_id
+        self.android_appname = android_appname
+        self.android_package = android_package
+        self.save()
+
+    def update_android_appname(self, android_appname):
+        self.android_appname = android_appname
+        self.save()
